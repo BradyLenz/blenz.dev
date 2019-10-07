69064b02569a8467e6153f466ae8303022e6b4cd
Brady Lenz <bradylenz98@gmail.com>
2019-10-07 01:37:26 -0500

    feat: content util

---
+++ blenz.dev/src/util/data.ts
@@ -0,0 +1,22 @@
+export const content = [
+    {
+        title: 'About Me',
+        headerContent: 'About',
+        link: 'about',
+    },
+    {
+        title: 'Work Experience',
+        headerContent: 'Experience',
+        link: 'experience',
+    },
+    {
+        title: 'Projects',
+        headerContent: 'Projects',
+        link: 'projects',
+    },
+    {
+        title: 'Contact',
+        headerContent: 'Contact',
+        link: 'contact',
+    },
+];
