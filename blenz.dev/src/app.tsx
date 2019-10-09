4bb800a0026d36aac42ac17a4c503379f54c7d4e
Brady Lenz <bradylenz98@gmail.com>
2019-10-09 02:13:16 -0500

    feat: app styling

--- blenz.dev/src/app.tsx
+++ blenz.dev/src/app.tsx
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Home } from './components/home';
 import { theme } from './theme';
+import { Box } from '@material-ui/core';
 
 export const App = () => {
     return (
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <Home/>
+                <Box bgcolor='#F7F9FB'>
+                    <Home/>
+                </Box>
             </ThemeProvider>
         </BrowserRouter>
     );
