317803925b3abd0bdd27d1547f30f8e88ffe5673
Brady Lenz <bradylenz98@gmail.com>
2019-10-10 21:14:09 -0500

    feat: added footer

---
+++ src/components/shared/footer/footer.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {
+    Grid,
+    makeStyles,
+    IconButton,
+    createStyles,
+    Typography,
+    Link
+} from '@material-ui/core';
+
+import { globals } from '../../../util';
+import {
+    GitHub,
+    LinkedIn,
+} from '../../shared/icons';
+
+const useStyles = makeStyles((theme) => createStyles({
+    root: {
+        height: 64,
+        background: theme.palette.background.default,
+    },
+}));
+
+export const Footer = () => {
+    const classes = useStyles();
+
+    return (
+        <Grid
+            container
+            justify='center'
+            alignItems='center'
+            alignContent='center'
+            direction='column'
+            className={classes.root}
+        >
+            <Grid item>
+                <Grid
+                    container
+                    justify='center'
+                >
+                    <Grid item>
+                        <IconButton
+                            size='small'
+                            component={Link}
+                            href={globals.linkedin}
+                            target='_blank'
+                        >
+                            <GitHub color='secondary'/>
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        <IconButton
+                            size='small'
+                            component={Link}
+                            href={globals.linkedin}
+                            target='_blank'
+                        >
+                            <LinkedIn color='secondary'/>
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item>
+                <Typography
+                    color='secondary'
+                    variant='body1'
+                >
+                    {globals.name}
+                </Typography>
+            </Grid>
+        </Grid>
+    );
+};
