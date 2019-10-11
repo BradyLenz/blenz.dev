65c144f58ce235f94175bad7fa35cea3308f727d
Brady Lenz <bradylenz98@gmail.com>
2019-10-10 21:15:02 -0500

    feat: home data

--- src/util/data.tsx
+++ src/util/data.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import {
+    About,
+    Contact,
+    Experience,
+    Projects,
+} from '../components/home';
+
 export interface ISection {
     title: string;
     headerContent: string;
     link: string;
-    element?: () => React.ReactElement;
+    element: (props: ISection) => React.ReactElement;
 }
 
 export const content: ISection[] = [
@@ -12,20 +19,24 @@ export const content: ISection[] = [
         title: 'About Me',
         headerContent: 'About',
         link: 'about',
+        element: (props: ISection) => <About {...props}/>,
     },
     {
         title: 'Work Experience',
         headerContent: 'Experience',
         link: 'experience',
+        element: (props: ISection) => <Experience {...props}/>,
     },
     {
         title: 'Projects',
         headerContent: 'Projects',
         link: 'projects',
+        element: (props: ISection) => <Projects {...props}/>,
     },
     {
-        title: 'Contact',
+        title: 'Contact Me',
         headerContent: 'Contact',
         link: 'contact',
+        element: (props: ISection) => <Contact {...props}/>,
     },
 ];
--- src/util/globals.ts
+++ src/util/globals.ts
@@ -9,7 +9,10 @@ interface IGlobals {
 export const globals: IGlobals = {
     greeting: 'Hi, my name is',
     name: 'Brady Lenz',
-    description: 'I am a software engineer from Madison, WI.',
+    description: `
+    I am a software engineer from Madison, WI with particular interests
+    in machine learning and optimization.
+    `,
     github: 'https://github.com/bradylenz',
     linkedin: 'https://www.linkedin.com/in/brlenz/',
 };
