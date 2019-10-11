df068b417a8ee231dc38b8b501c186d86544b92b
Brady Lenz <bradylenz98@gmail.com>
2019-10-10 21:13:23 -0500

    feat: responsive text sizes

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,9 @@
-import { createMuiTheme } from '@material-ui/core';
+import {
+    createMuiTheme,
+    responsiveFontSizes,
+} from '@material-ui/core';
 
-export const theme = createMuiTheme({
+export const paletteTheme = createMuiTheme({
     palette: {
         primary: {
             main: '#31708E',
@@ -13,10 +16,51 @@ export const theme = createMuiTheme({
         },
         type: 'light',
     },
+});
+
+export const theme = responsiveFontSizes(createMuiTheme({
+    palette: paletteTheme.palette,
     typography: {
         h1: {
             fontWeight: 600,
+            [paletteTheme.breakpoints.up('md')]: {
+                fontSize: '6rem',
+            },
+            [paletteTheme.breakpoints.down('md')]: {
+                fontSize: '5rem',
+            },
+            [paletteTheme.breakpoints.down('sm')]: {
+                fontSize: '4rem',
+            },
+            [paletteTheme.breakpoints.down('xs')]: {
+                fontSize: '3rem',
+            },
+        },
+        h2: {
+            fontWeight: 500,
+        },
+        h3: {
+            fontWeight: 500,
+        },
+        h4: {
+            fontWeight: 500,
+            color: paletteTheme.palette.text.primary,
+        },
+        h5: {
+            fontWeight: 500,
+        },
+        h6: {
+            fontWeight: 500,
+        },
+        body1: {
+            color: paletteTheme.palette.text.hint,
+        },
+        body2: {
+            color: paletteTheme.palette.text.hint,
         },
     },
     spacing: 4,
-});
+    zIndex: {
+        appBar: 1400,
+    },
+}));
