ecb4251f47074d85a920a8288377296114c344c1
Brady Lenz <bradylenz98@gmail.com>
2019-10-13 22:31:41 -0500

    feat: contact section

--- src/components/home/contact.tsx
+++ src/components/home/contact.tsx
@@ -1,15 +1,64 @@
 import React from 'react';
+import {
+    Typography,
+    Button,
+    makeStyles,
+    Grid,
+    Link,
+} from '@material-ui/core';
 
-import { ISection } from '../../util';
+import {
+    ISection,
+    contactData,
+} from '../../data';
 import {
     SuperCenter,
     HeaderHR,
 } from '../shared';
 
+const useStyles = makeStyles({
+    title: {
+        marginBottom: 40,
+    },
+    message: {
+        maxWidth: 450,
+        marginBottom: 40,
+    },
+    emailButton: {
+        margin: 'auto',
+    },
+});
+
 export const Contact = (props: ISection) => {
+    const classes = useStyles();
+
     return (
         <SuperCenter>
-            <HeaderHR text={props.title}/>
+            <HeaderHR
+                text={props.title}
+                className={classes.title}
+            />
+            <Typography
+                align='center'
+                className={classes.message}
+            >
+                {contactData.message}
+            </Typography>
+            <Grid
+                container
+                justify='center'
+            >
+                <Grid item>
+                    <Button
+                        variant='contained'
+                        size='large'
+                        color='primary'
+                        className={classes.emailButton}
+                    >
+                        {contactData.button.text}
+                    </Button>
+                </Grid>
+            </Grid>
         </SuperCenter>
     );
 };
